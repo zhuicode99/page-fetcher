e52b281d570bc5e385b97d48884c7ecd93c86544
zhuicode99 <enlin291814833@hotmail.com>
2022-09-06 00:50:24 +0000

    minor change

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,13 @@
-const request = require('request');
-const fs = require("fs");
+const request = require('request');//npm install request
+const fs = require("fs"); // write data into local filesystem
 
 const targetFile = process.argv.slice(2)[1];
 const url = process.argv.slice(2)[0];
 
+
 request(url, (error, response, body) => {
-  if (Number(response) > 299 || !response) {
-    console.log('Error: ' + error);
+  if (Number(response.statusCode) > 299 || !response) {
+    console.log('Error: ' + error + '\nPlease try again later');
   }
   if (error) {
     console.log('Error: ' + error + '\nPlease make sure your url is correct');
